43d71737b572d70b4bb0d9dda988c4f41d33c8ba
Dan McClain <git@danmcclain.net>
2013-04-26 10:49:13 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RealtimeRails::Application.config.secret_key_base = 'b6c095fb63e3f01a784ee70ddc77fd90c9f43dad80f7a5e8cdf0606c966d1ee5dc5d7b63f6764828b412de8830e2c878a725d5ee752996daaec54644e2e21ebb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RealtimeRails::Application.config.session_store :encrypted_cookie_store, key: '_realtime-rails_session'
